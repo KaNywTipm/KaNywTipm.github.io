4b08d418fc6630f99acf5e16f4143d1daecc606c
Kanyawee <66025997@up.ac.th>
2024-11-29 21:31:23 +0700

    footer&scroll all

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,33 @@ const sendEmail = (e) => {
 contactForm.addEventListener('submit', sendEmail);
 
 /*========== Show scroll up ===========*/
+const scrollup =() =>{
+      const scrollUp = document.getElementById('scroll-up')
+      this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                  : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollup)
 
 /*========== Scroll section active link ===========*/
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () => {
+      const scrollDown = window.scrollY;
+
+      sections.forEach(current => {
+            const sectionHeight = current.offsetHeight,
+                  sectionTop = current.offsetTop - 58,
+                  sectionId = current.getAttribute('id'),
+                  sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']');
+
+            if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+                  sectionsClass.classList.add('active-link');
+            } else {
+                  sectionsClass.classList.remove('active-link');
+            }
+      });
+};
+window.addEventListener('scroll', scrollActive);
 
 /*========== Scroll reveal animation ===========*/
